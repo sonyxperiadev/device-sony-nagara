172a12d416744fdf6a0e792955d8142152b42891
Konrad Dybcio <konrad.dybcio@somainline.org>
2022-08-16 02:29:49 +0200

    platform.mk: Import common GKI / boot overlays
    [1] https://source.android.com/docs/core/bootloader/partitions/generic-boot#product-configurations
    [2] https://source.android.com/docs/core/bootloader/partitions/generic-boot#making-e2fsck-available-2

--- platform.mk
+++ platform.mk
@@ -429,3 +429,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, device/sony/common/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
