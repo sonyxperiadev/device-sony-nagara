sonyxperiadev/device-sony-nagara

commit bd70796109e0054bc47f9e7df739c19069438b4e

platform: add keymint-V1-ndk_platform
vendor blobs need android.hardware.security.keymint-V1-ndk_platform
to be able to load

Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

platform.mk
@@ -250,6 +250,7 @@ PRODUCT_PACKAGES += \
 # Keymaster 4 passthrough service init file
 # (executable is on odm)
 PRODUCT_PACKAGES += \
+    android.hardware.security.keymint-V1-ndk_platform \
     android.hardware.keymaster@4.1-service-qti.rc \
     android.hardware.keymaster@4.1.vendor \
     android.hardware.security.keymint-service-qti.rc \

commit eaef022c30cb20f9e1a95dd891e4b60ee9fec155

platform: Move `android.hardware.security.keymint` AIDL lib to `/vendor`
The `ndk_platform` libraries end up on the `/system` partition, but we
need them on the `/vendor` partition to be available from the vndk for
this `keymint` QTI ODM binary to load them:

    I init    : starting service 'vendor.keymint-qti'...
    F linker  : CANNOT LINK EXECUTABLE "/odm/bin/hw/android.hardware.security.keymint-service-qti": library "android.hardware.security.keymint-V1-ndk_platform.so" not found: needed by main executable
    I init    : Service 'vendor.keymint-qti' (pid 5298) exited with status 1
    I init    : Sending signal 9 to service 'vendor.keymint-qti' (pid 5298) process group...

Note that the `ndk_platform` target will be removed in the future, so we
should seek to switch this back to just an `ndk` dependency.

Fixes: bd70796 ("platform: add keymint-V1-ndk_platform")
Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

platform.mk
@@ -246,7 +246,7 @@ PRODUCT_PACKAGES += \
 # Keymaster 4 passthrough service init file
 # (executable is on odm)
 PRODUCT_PACKAGES += \
-    android.hardware.security.keymint-V1-ndk_platform \
+    android.hardware.security.keymint-V1-ndk_platform.vendor \
     android.hardware.keymaster@4.1-service-qti.rc \
     android.hardware.keymaster@4.1.vendor \
     android.hardware.security.keymint-service-qti.rc \