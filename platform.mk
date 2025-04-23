07c8c494b0277c06af65c6dc1d6e28a0ac979d2f
Alin Jerpelea <alin.jerpelea@sony.com>
2025-04-23 15:13:12 +0200

    platform.mk: add camera configuration
    nagara uses camera provider 2.7
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -89,6 +89,10 @@ TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 TARGET_USES_DRM_PP := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 2
 
+# Camera
+TARGET_USES_QTI_CAMERA := true
+QTI_CAMERA_PROVIDER_SERVICE := 2.7
+
 # TEMP: Disable Fingerprint HAL until support for this platform
 # lands in HAL. Fixes incredible CPU usage:
 # https://github.com/sonyxperiadev/bug_tracker/issues/860
